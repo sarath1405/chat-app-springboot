e09dd41f7627b4d208d4ca0a7b7343504586b867
sarath1405 <sarathchandra1426@gmail.com>
2021-12-06 19:10:12 +0530

    commit1

---
+++ chat-app/src/main/java/com/sarath/chatapp/services/SampleService.java
@@ -0,0 +1,11 @@
+package com.sarath.chatapp.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+    
+    public String getEmployee() {
+        return "venkatesh";
+    }
+}
